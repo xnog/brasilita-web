edef2341c5248ac080d9e20dc8c3aae0b736dfa8
Felipe Nogueira Gaudio <xnogueira@gmail.com>
2025-08-11 21:48:50 +0200

    remove custom cookies

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,37 +54,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     ],
     session: {
         strategy: "jwt",
-        maxAge: 30 * 24 * 60 * 60, // 30 dias
-        updateAge: 24 * 60 * 60, // 24 horas
-    },
-    cookies: {
-        sessionToken: {
-            name: "__Secure-next-auth.session-token",
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                secure: true,
-            },
-        },
-        callbackUrl: {
-            name: "__Secure-next-auth.callback-url",
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                secure: true,
-            },
-        },
-        csrfToken: {
-            name: "__Host-next-auth.csrf-token",
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                secure: true,
-            },
-        },
     },
     pages: {
         signIn: "/auth/signin",
