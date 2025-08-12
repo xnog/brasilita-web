f9a97e5e4934b4aaa5e75583a629a7e1cdde4edc
Felipe Nogueira Gaudio <xnogueira@gmail.com>
2025-08-12 09:43:01 +0200

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -15,6 +15,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            checks: ["state"],
         }),
         Credentials({
             name: "credentials",
